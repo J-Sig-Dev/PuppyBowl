c31d68619794097de9db5b410131e966d79a8c07
Jimmy Signature <jimmysignature@gmail.com>
2023-06-02 21:52:46 -0400

    retrieve and display all players

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const playerContainer = document.getElementById('all-players-container')
 const newPlayerFormContainer = document.getElementById('new-player-form')
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'YOUR COHORT NAME HERE'
+const cohortName = '2302-ACC-PT-WEB-PT-A'
+
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`
 
@@ -12,6 +13,10 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`
  */
 const fetchAllPlayers = async () => {
   try {
+    const response = await fetch(`${APIURL}players`)
+    const result = await response.json()
+    console.log(result)
+    return result
   } catch (err) {
     console.error('Uh oh, trouble fetching players!', err)
   }
@@ -59,12 +64,24 @@ const removePlayer = async playerId => {
  *
  * The `fetchSinglePlayer` and `removePlayer` functions are defined in the
  * @param playerList - an array of player objects
- * @returns the playerContainerHTML variable.
+ * @returns the playerontainerHTML variable.
  */
-const renderAllPlayers = playerList => {
+const renderAllPlayers = async playerList => {
   try {
+    const data = playerList.data.players
+    playerContainer.innerHTML = ''
+    data.forEach(player => {
+      const playerDiv = document.createElement('div')
+      playerDiv.classList.add('card')
+      playerDiv.innerHTML = `<h1>${player.name}<h1>
+      <img  src= ${player.imageUrl}>
+      <p>${player.breed}<p>
+      <p>${player.teamId}<p>`
+
+      playerContainer.appendChild(playerDiv)
+    })
   } catch (err) {
-    console.error('Uh oh, trouble rendering players!', err)
+    console.log('Uh oh, trouble rendering players!', err)
   }
 }
 
@@ -81,9 +98,10 @@ const renderNewPlayerForm = () => {
 
 const init = async () => {
   const players = await fetchAllPlayers()
+  //   console.log(players)
   renderAllPlayers(players)
 
-  renderNewPlayerForm()
+  //   renderNewPlayerForm()
 }
 
 init()
